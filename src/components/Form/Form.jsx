957223762159bac58be4e89fc3a0086a3697cdda
Vlad <tyronvv@krok.edu.ua>
2023-06-15 16:53:04 +0300

    14

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import './Form.css';
 
 
 const Form = () => {
+
+    const[city, setCity] = useState('');
+    const[street, setStreet] = useState('');
+    const[number, setNumber] = useState('');
+    const[name, setName] = useState('');
+
+    const onChangeCity = (e) => {
+        setCity(e.target.value)
+    }
+
+    const onChangeStreet = (e) => {
+        setStreet(e.target.value)
+    }
+
+    const onChangeNumber = (e) => {
+        setNumber(e.target.value)
+    }
+
+    const onChangeName = (e) => {
+        setName(e.target.value)
+    }
+
     return(
-        <div>
-            Form
+        <div className={"form"}>
+            <h3>Заповніть ваші дані</h3>
+            <input className={"input"} type='text' placeholder="Місто" value={city} onChange={onChangeCity}/>
+            <input className={"input"} type='text' placeholder="Вулиця" value={street} onChange={onChangeStreet}/>
+            <input className={"input"} type='text' placeholder="Номер телефону" value={number} onChange={onChangeNumber}/>
+            <input className={"input"} type='text' placeholder="ФІО" value={name} onChange={onChangeName}/>
         </div>
     );
 };
