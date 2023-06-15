63283ba6e7acb66be202e20c66ec0f535d14c61c
Vlad <tyronvv@krok.edu.ua>
2023-06-15 17:55:28 +0300

    111

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
     return (
         <div className="App">
             <Header />
+            <button>button</button>
             <Routes>
                 <Route index element={<ProductList />}/>
                 <Route path={'form'} element={<Form />}/>
